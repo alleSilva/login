17913470c742374d63abb9471aaffd4f59a29f98
Alesandro Silva <silva_alesandro@outlook.com>
2022-09-29 11:42:49 -0400

    config fairebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login/login_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+class DefaultFirebaseOptions {}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
